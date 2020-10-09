9417fd2bc700d2043d9c583bf67756e86cedc855
Tenebrosful <hugobernard39@gmail.com>
2020-10-09 11:35:37 +0200

    Ajout de la fonction where

--- src/Query/Query.php
+++ src/Query/Query.php
@@ -6,13 +6,31 @@ class Query
 {
     private $sqltable;
     private $fields = '*';
-    private $where = null;
+    private $where = [];
     private $args = [];
     private $sql = '';
 
+    /**
+     * @param string $nomTable
+     * @return Query
+     */
     public static function table(string $nomTable){
         $query = new Query();
         $query->sqltable = $nomTable;
         return $query;
+    }
+
+    /**
+     * @param string $colonne
+     * @param string $comparateur
+     * @param mixed $valeur
+     * @return Query
+     */
+    public function where(string $colonne, string $comparateur, $valeur){
+        $this->where[]['col'] = $colonne;
+        $this->where[]['op'] = $comparateur;
+        $this->where[]['val'] = $valeur;
+        $this->args[] = $valeur;
+        return $this;
     }
 }
